cadfba4e7a09ba67df6dd3ee191694ffec2455c4
f4ww4z <fawwazyusran@gmail.com>
2026-02-16 03:52:12 +0100

    Image should cover entire screen for DoNotDisturbScreen

--- components/DoNotDisturbScreen.tsx
+++ components/DoNotDisturbScreen.tsx
@@ -15,17 +15,16 @@ const DoNotDisturbScreen = ({ onClick }: { onClick?: () => void }) => {
   return (
     <div
       onClick={onClick}
-      className="fixed z-50 flex items-center justify-center w-screen h-screen text-gray-300 bg-black"
+      className="fixed inset-0 z-50 flex items-center justify-center w-screen h-screen text-gray-300 bg-black"
     >
-      <p className="absolute w-full text-2xl font-semibold text-center bottom-14">
+      <p className="absolute w-full text-2xl font-semibold text-center bottom-14 z-10">
         {moment(time).format("HH:mm")}
       </p>
       <Image
         src="/mode solat.png"
         alt="Do Not Disturb"
-        width={1920}
-        height={1080}
-        className="w-auto h-full"
+        fill
+        className="object-cover"
       />
     </div>
   )
